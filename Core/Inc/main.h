22eba5e772cb8f2f6946a1ed7c698078af4ae74f
Vítor Caitité <vitorgabriel1000@hotmail.com>
2020-09-22 21:35:05 -0300

    Setup inicial - CUBE MX

---
+++ Core/Inc/main.h
@@ -0,0 +1,98 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define SW1_Pin GPIO_PIN_1
+#define SW1_GPIO_Port GPIOA
+#define SW1_EXTI_IRQn EXTI1_IRQn
+#define SW2_Pin GPIO_PIN_4
+#define SW2_GPIO_Port GPIOA
+#define SW2_EXTI_IRQn EXTI4_IRQn
+#define LED_D1_Pin GPIO_PIN_5
+#define LED_D1_GPIO_Port GPIOA
+#define LED_D2_Pin GPIO_PIN_6
+#define LED_D2_GPIO_Port GPIOA
+#define LED_D3_Pin GPIO_PIN_7
+#define LED_D3_GPIO_Port GPIOA
+#define SW3_Pin GPIO_PIN_0
+#define SW3_GPIO_Port GPIOB
+#define SW3_EXTI_IRQn EXTI0_IRQn
+#define SRCLK_Pin GPIO_PIN_8
+#define SRCLK_GPIO_Port GPIOA
+#define SER_Pin GPIO_PIN_9
+#define SER_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define RCLK_Pin GPIO_PIN_5
+#define RCLK_GPIO_Port GPIOB
+#define LED_D4_Pin GPIO_PIN_6
+#define LED_D4_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
